7319c61a8d0757e43bbaa6d9d21a998d244d10b0
sarmaasis <sarmaasis@gmail.com>
2025-05-17 00:17:27 +0530

    ssr config fix

--- src/server.tsx
+++ src/server.tsx
@@ -1,12 +1,16 @@
-import { renderToString } from 'react-dom/server';
-import App from './App';
+import { renderToString } from 'react-dom/server'
+import App from './App'
+import manifest from '../dist/client/.vite/manifest.json' // <-- Add this
 
 export function render() {
+  // Find the client entry file in the manifest
+  const clientEntry = manifest['src/client.tsx'].file
   return `<!DOCTYPE html>
 <html>
   <head><title>My SSR App</title></head>
   <body>
     <div id="root">${renderToString(<App />)}</div>
+    <script type="module" src="/assets/${clientEntry}"></script>
   </body>
-</html>`;
+</html>`
 }
