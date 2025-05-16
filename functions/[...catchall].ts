18ac96e75648a0c07d2c8f9665898a8e845f384f
sarmaasis <sarmaasis@gmail.com>
2025-05-16 22:49:13 +0530

    cf congig fix imports

--- functions/[...catchall].ts
+++ functions/[...catchall].ts
@@ -1,9 +1,9 @@
-import { PagesFunction, Response as WorkerResponse } from '@cloudflare/workers-types';
+import { PagesFunction, Response} from '@cloudflare/workers-types';
 import { render } from '../dist/server/server.js'; // SSR render function
 
 export const onRequest: PagesFunction = async (context) => {
   const html = await render();
-  return new WorkerResponse(html, {
+  return new Response(html, {
     headers: { 'Content-Type': 'text/html' },
   });
 };
