c22cf642b0a9f7d88e96e7663afcbec442e352a3
sarmaasis <sarmaasis@gmail.com>
2025-05-17 01:24:08 +0530

    test full app for ssr

--- src/server.tsx
+++ src/server.tsx
@@ -1,33 +1,22 @@
-// import { renderToString } from 'react-dom/server';
-// import manifest from '../dist/client/.vite/manifest.json';
-// import App from './App';
+import { renderToString } from 'react-dom/server';
+import manifest from '../dist/client/.vite/manifest.json';
+import App from './App';
 
 export function render() {
-//   const clientEntry = manifest['src/client.tsx'].file;
-//   const cssFiles = manifest['src/client.tsx'].css || [];
-//   const cssLinks = cssFiles.map(
-//     (href) => `<link rel="stylesheet" href="/${href}">`
-//   ).join('\n');
+  const clientEntry = manifest['src/client.tsx'].file;
+  const cssFiles = manifest['src/client.tsx'].css || [];
+  const cssLinks = cssFiles.map(
+    (href) => `<link rel="stylesheet" href="/${href}">`
+  ).join('\n');
   return `<!DOCTYPE html>
 <html>
-
-<html>
-  <head>
-    <title>SSR Test</title>
+ <head>
+    <title>My SSR App</title>
+    ${cssLinks}
   </head>
   <body>
-    <div id="root">SSR TEST</div>
+    <div id="root">${renderToString(<App />)}</div>
+    <script type="module" src="/${clientEntry}"></script>
   </body>
- 
 </html>`;
 }
-
-
-//  <head>
-//     <title>My SSR App</title>
-//     ${cssLinks}
-//   </head>
-//   <body>
-//     <div id="root">${renderToString(<App />)}</div>
-//     <script type="module" src="/${clientEntry}"></script>
-//   </body>
