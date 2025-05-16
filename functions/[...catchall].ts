2b8231223a04c2e5bff20f2e99730fc6706ba71c
sarmaasis <sarmaasis@gmail.com>
2025-05-16 23:15:05 +0530

    page function fix

--- functions/[...catchall].ts
+++ functions/[...catchall].ts
@@ -1,7 +1,7 @@
-import { PagesFunction, Response} from '@cloudflare/workers-types';
+import { PagesFunction } from '@cloudflare/workers-types';
 import { render } from '../dist/server/server.js'; // SSR render function
 
-export const onRequest: PagesFunction = async (context) => {
+export const onRequest = async (context) => {
   const html = await render();
   return new Response(html, {
     headers: { 'Content-Type': 'text/html' },
