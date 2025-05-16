75533f151ea6f6715df7eb8c300a6451467b8752
sarmaasis <sarmaasis@gmail.com>
2025-05-17 01:02:12 +0530

    test plain text _

--- src/server.tsx
+++ src/server.tsx
@@ -1,13 +1,13 @@
-import { renderToString } from 'react-dom/server';
-import manifest from '../dist/client/.vite/manifest.json';
-import App from './App';
+// import { renderToString } from 'react-dom/server';
+// import manifest from '../dist/client/.vite/manifest.json';
+// import App from './App';
 
 export function render() {
-  const clientEntry = manifest['src/client.tsx'].file;
-  const cssFiles = manifest['src/client.tsx'].css || [];
-  const cssLinks = cssFiles.map(
-    (href) => `<link rel="stylesheet" href="/${href}">`
-  ).join('\n');
+//   const clientEntry = manifest['src/client.tsx'].file;
+//   const cssFiles = manifest['src/client.tsx'].css || [];
+//   const cssLinks = cssFiles.map(
+//     (href) => `<link rel="stylesheet" href="/${href}">`
+//   ).join('\n');
   return `<!DOCTYPE html>
 <html>
 
