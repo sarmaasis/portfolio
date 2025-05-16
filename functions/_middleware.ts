14a12d7bb2bf847894798e5646c7b6f108d33973
sarmaasis <sarmaasis@gmail.com>
2025-05-17 01:30:11 +0530

    fix js and css in ssr

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,6 +1,13 @@
 import { render } from '../dist/server/server.js'; // SSR render function
 
 export const onRequest = async (context) => {
+  
+  const url = new URL(context.request.url);
+  // If the request is for an asset, let Cloudflare serve it
+  if (url.pathname.startsWith('/assets/') || url.pathname.startsWith('/favicon') || url.pathname.endsWith('.css')) {
+    return context.next();
+  }
+
   const html = await render();
   return new Response(html, {
     headers: { 'Content-Type': 'text/html' },
